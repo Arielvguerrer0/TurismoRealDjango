f50e587f13d2c50b1c778b898e51909684324bab
Diego Campos <di.campos@duocuc.cl>
2022-09-05 22:29:50 -0300

    Api

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import * #importa todas las vistas
+from rest_framework import routers #para importar los serializers creados y ser vistos
+
+router = routers.DefaultRouter()#permite crear urls para las api
+router.register('ciudad', CiudadViewset)
 
 urlpatterns = [
     path('', home, name="home"),
+    path('api/', include(router.urls)),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import *
+from rest_framework import viewsets
+from .serializers import *
+
+# se crean clases para las api
+
+class CiudadViewset(viewsets.ModelViewSet):#este se encarga de mostrar los datos y hasta guardar
+    queryset = Ciudad.objects.all()
+    serializer_class = CiudadSerializer
 
 # se crean las vistas (programación).
 
